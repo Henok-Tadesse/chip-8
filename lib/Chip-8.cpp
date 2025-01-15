56052aeee561d78b1351055d28d383402613c46a
Henok-Tadesse <140813166+Henok-Tadesse@users.noreply.github.com>
2025-01-15 11:19:35 +0100

    add placeholders for all instructions

--- lib/Chip-8.cpp
+++ lib/Chip-8.cpp
@@ -114,15 +114,117 @@ void Chip8::fillDisplaybuffer(int x, int y, int n) {
 void Chip8::decode() {
 
   switch (opcode & 0xF000) {
-  case 0xA000: // 0xAnnn
-    printf("manupilating index\n");
-    I = opcode & 0x0FFF;
+  case 0x0000:
+    switch (opcode & 0x000F) {
+    case 0x0000: // 0x00E0 , clear the screen
+      printf("clear the screen\n");
+      clearDisplay();
+      pc = pc + 2;
+      break;
+    case 0x00E0: // 0x00EE: returns from subroutine
+
+      printf("returns from subroutine\n");
+      break;
+    default:
+      printf("don't know shit man\n");
+      break;
+    }
+    break;
+  case 0x1000:
+    printf("Jump to location nnn.\n");
+    pc = opcode & 0x0FFF;
+    break;
+  case 0x2000:
+    printf("Call subroutine at nnn.");
+    stack[sp] = pc;
+    sp++;
+    pc = opcode & 0x0FFF;
+    break;
+  case 0x3000:
+    printf("Skip next instruction if Vx = kk.");
+    // compare V[x]==kk and if that's the case, simply increment by 4
+    if (V[opcode & 0x0F00] == opcode & 0x00FF) {
+      pc = pc + 4;
+      break;
+    }
+    pc = pc + 2;
+    break;
+  case 0x4000:
+    printf("Skip next instruction if Vx != kk.");
+    // compare V[x]!=kk and if that's the case, simply increment by 4
+    if (V[opcode & 0x0F00] != opcode & 0x00FF) {
+      pc = pc + 4;
+      break;
+    }
+    pc = pc + 2;
+    break;
+  case 0x5000:
+    printf("Skip next instruction if Vx == Vy.");
+    // compare V[x]==V[y] and if that's the case, simply increment by 4
+    if (V[opcode & 0x0F00] == opcode & 0x00F0) {
+      pc = pc + 4;
+      break;
+    }
     pc = pc + 2;
     break;
   case 0x6000:
     printf("Set Vx = kk\n");
     V[(opcode & 0x0F00) >> 8] = opcode & 0x00FF;
     pc = pc + 2;
+    break;
+  case 0x7000:
+    printf("Set Vx = Vx + kk\n");
+    V[(opcode & 0x0F00) >> 8] = (opcode & 0x00FF) + V[(opcode & 0x0F00) >> 8];
+    pc = pc + 2;
+    break;
+  case 0x8000:
+    switch (opcode & 0x000F) {
+    case 0x0000: // 0x8xy0
+      printf("Set Vx = Vy.");
+      V[opcode & 0x0F00] = V[opcode & 0x0F00];
+      pc = pc + 2;
+      break;
+    case 0x0001: // 0x8xy1
+      printf("Set Vx = Vx OR Vy.\n");
+      break;
+    case 0x0002: // 0x8xy2
+      printf("Set Vx = Vx AND Vy.\n");
+      break;
+    case 0x0003: // 0x8xy3
+      printf("Set Vx = Vx XOR Vy.\n");
+      break;
+    case 0x0004: // 0x8xy4
+      printf("Set Vx = Vx + Vy\n");
+      break;
+    case 0x0005:
+      printf("Set Vx = Vx - Vy, set VF = NOT borrow.");
+      break;
+    case 0x0006: // 0x8xy5
+      printf("Set Vx = Vx SHR 1.\n");
+      break;
+    case 0x0007: // 0x8xy6
+      printf("Set Vx = Vy - Vx, set VF = NOT borrow.\n");
+      break;
+    case 0x000E: // 0x8xyE
+      printf("Set Vx = Vx SHL 1.\n");
+      break;
+    default:
+      break;
+    }
+    break;
+  case 0x9000:
+    printf("Skip next instruction if Vx != Vy.");
+    break;
+  case 0xA000: // 0xAnnn
+    printf("Set I = nnn.\n");
+    I = opcode & 0x0FFF;
+    pc = pc + 2;
+    break;
+  case 0xB000: // 0xAnnn
+    printf("Jump to location nnn + V0.\n");
+    break;
+  case 0xC000: // Cxkk
+    printf("Set Vx = random byte AND kk.\n");
     break;
   case 0xD000:
     printf("Display n-byte sprite starting at memory location\n");
@@ -136,30 +238,66 @@ void Chip8::decode() {
     drawFlag = true;
     pc = pc + 2;
     break;
-  case 0x7000:
-    printf("Set Vx = Vx + kk\n");
-    V[(opcode & 0x0F00) >> 8] = (opcode & 0x00FF) + V[(opcode & 0x0F00) >> 8];
-
-    pc = pc + 2;
-    break;
-  case 0x0000:
-    switch (opcode & 0x000F) {
-    case 0x0000: // 0x00E0 , clear the screen
-      printf("clear the screen\n");
-      clearDisplay();
-      pc = pc + 2;
+  case 0xE000:
+    switch (opcode & 0x00F0) {
+    case 0x009E:
+      printf("Skip next instruction if key with the value of Vx is pressed.\n");
       break;
-    case 0x00E0: // 0x00EE: returns from subroutine
-      printf("returns from subroutine\n");
+    case 0x00A1:
+      printf("Skip next instruction if key with the value of Vx is not "
+             "pressed.\n");
       break;
     default:
-      printf("don't know shit man\n");
       break;
     }
     break;
-  case 0x1000:
-    printf("Jump to location nnn.\n");
-    pc = opcode & 0x0FFF;
+  case 0xF000:
+    switch (opcode & 0x00F0) {
+    case 0x0000:
+      switch (opcode && 0x000F) {
+      case 0x0007:
+        printf("Set Vx = delay timer value.");
+        break;
+      case 0x000A:
+        printf("Wait for a key press, store the value of the key in Vx.");
+        break;
+      default:
+        break;
+      }
+      break;
+    case 0x0010:
+      switch (opcode && 0x000F) {
+      case 0x0005:
+        printf("Set delay timer = Vx.");
+        break;
+      case 0x0008:
+        printf("Set sound timer = Vx.");
+        break;
+      case 0x000E:
+        printf("Set I = I + Vx.");
+        break;
+      default:
+        break;
+      }
+      break;
+    case 0x0020:
+      printf("Set I = location of sprite for digit Vx.\n");
+      break;
+    case 0x0030:
+      printf("Store BCD representation of Vx in memory locations I, I+1, and "
+             "I+2.\n");
+      break;
+    case 0x0050:
+      printf(
+          "Store registers V0 through Vx in memory starting at location I.\n");
+      break;
+    case 0x0060:
+      printf(
+          "Read registers V0 through Vx from memory starting at location I.\n");
+      break;
+    default:
+      break;
+    }
     break;
   default:
     printf("opcode not implemented yet\n");
